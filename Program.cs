b1251013f6a81809181c35e0f72dc9fe3f5647c5
wwwbalad55-rgb <wwwbalad55@gmail.com>
2026-05-06 01:16:40 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CarRentalSystem
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+using System;
+namespace CarRentalSystem {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("CI Build Success!");
         }
     }
 }
